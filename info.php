e7a82efa1158d5c675bf6ec9fd8e95c81b0edfec
Mark Dominguez <tnemexico@gmail.com>
2015-11-13 18:55:56 -0300

    Create info.php

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+	//DATOS DEL PROGRAMA==================================
+	$CLIENTE= "MS Solutions" ;
+	$SISTEMA = "Analyzzer CFDI Jr. + Inventarios" ;
+	$VERSION = "Version 2.0 MS Solutions" ;
+	//$NICK = "Por Lic. Marco A. Dominguez" ;
+	//========================================================
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+	<title>
+<?php echo "$CLIENTE $SISTEMA $VERSION"; ?>
+	</title>
+<?php 	require 'conectar.php'; ?>
+</head>
+<body>
